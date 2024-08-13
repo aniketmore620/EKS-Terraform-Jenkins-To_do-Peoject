a5cf3d3f9be562d6c0afe050ab9990780294664b
aniketmore620 <145662770+aniketmore620@users.noreply.github.com>
2024-08-13 17:10:16 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,20 +1,9 @@
 terraform {
-  required_version = "~> 1.9.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.49.0"
-    }
-  }
   backend "s3" {
-    bucket         = "my-ews-aniket"
+    bucket         = "your-s3-bucket-name"
+    key            = "path/to/terraform.tfstate"
     region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
-}
-
-provider "aws" {
-  region = "us-east-1"  # Ensure this matches your desired region
 }
