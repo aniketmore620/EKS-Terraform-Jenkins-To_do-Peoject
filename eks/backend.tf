9cc6631a0853598fc2c7249985207630b096509d
aniketmore620 <145662770+aniketmore620@users.noreply.github.com>
2024-08-13 17:12:28 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-s3-bucket-name"
+    bucket         = "my-ews-aniket"
     key            = "path/to/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "Lock-Files"
